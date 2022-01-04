8ace2878cae9d5206099bf539655246d1155720a
Abbas Khan <78753275+Abbas-glitch@users.noreply.github.com>
2022-01-04 18:15:31 +0530

    Delete EmailApp directory

--- EmailApp/src/main/java/emailapplication/Main.java
+++
@@ -1,9 +0,0 @@
-package emailapplication;
-
-public class Main {
-    public static void main(String[] args) {
-    email em1 = new email(" Abbas","Khan");
-
-        System.out.println(em1.showInfo());
-    }
-}
--- EmailApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.emailapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.emailapp to javafx.fxml;
-    exports com.example.emailapp;
-}
